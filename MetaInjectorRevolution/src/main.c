20e8cb3b0e8099eb1af06e8c1495307d17504b91
CB205EDEE16B24366C87 <nyx>
2017-11-15 12:31:06 +0100

    Adding #ifdef on debug for system("pause");

--- MetaInjectorRevolution/src/main.c
+++ MetaInjectorRevolution/src/main.c
@@ -20,7 +20,9 @@ int main(int argc, char **argv)
 		printf("\nMake sure you place this program in the same folder as the pad00000.meta file.\n");
 		printf("\nIf it already is. Try running this program as administrator.\n");
 		printf("\nIf that doesn't work either. Move your entire game's folder to your desktop and try again.");
+#ifdef _DEBUG
 		system("pause"); // TODO Remove me
+#endif
 		return 1;
 	}
 
@@ -31,28 +33,34 @@ int main(int argc, char **argv)
 		printf("\t-autopatch : Path files in topatch directory without asking for confirmations\n");
 		printf("\t-restore : Remove last changes\n");
 		printf("\tmod.zip : Install a mod\n");
+#ifdef _DEBUG
 		system("pause"); // TODO Remove me
+#endif
 		return 1;
 	}
 
 	if (!strcmpi(argv[1], "-autopatch"))
 	{
 		int r = autoPatch();
+#ifdef _DEBUG
 		system("pause"); // TODO Remove me
+#endif
 		return r;
 	}
 	else if (!strcmpi(argv[1], "-restore"))
 	{
 		// TODO : Restaurer le fichier pad a avant le patch
-		system("pause"); // TODO Remove me
 	}
 	else
 	{ 
 		printf("Installing mod file %s\n", argv[1]);
 		// TODO: Check for zip extensions
 		printf("Not yet implemented\n");
-		system("pause"); // TODO Remove me
 	}
+
+#ifdef _DEBUG
+	system("pause");
+#endif
 
     return 0;
 }
